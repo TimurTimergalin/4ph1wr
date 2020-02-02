0eb48406059b504293d897a74d55e5ad06e20d31
TimurTimergalin <timur20041229@gmail.com>
2020-02-02 12:51:45 +0300

    Game separated finally

--- MyApp1.py
+++ MyApp1.py
@@ -203,7 +203,7 @@ class Game(FloatLayout):
                     i.working = False
                     i.background_color = (0, 0.3, 0, 1)
                     chosen.alpha_zero()
-                    self.lay.remove_widget(chosen)
+                    self.remove_widget(chosen)
                     i.clicked = True
                     i.cage = chosen
                     i.cage.filled = True
@@ -242,188 +242,30 @@ class Game(FloatLayout):
         WHERE answer = ?""", (self.word,))
         con.commit()
         con.close()
+        for i in self.cages:
+            self.remove_widget(i)
+        for i in self.letters:
+            self.remove_widget(i)
         self.new_word()
         self.help_button.working = True
+
+
+class Root(FloatLayout):
+    def __init__(self):
+        super().__init__()
+        self.game = Game()
+        self.start_game()
+
+    def start_game(self):
+        self.add_widget(self.game)
+
+    def end_game(self):
+        self.remove_widget(self.game)
 
 
 class MyApp(App):  # Приложение
     def build(self):
-        self.lay = FloatLayout()  # Корень
-        self.lay.add_widget(Image(source='data/background.png'))
-        self.help_button = HelpButton()
-        self.help_button.bind(on_press=self.help_callback)
-        self.lay.add_widget(self.help_button)
-        self.cages = []
-        self.letters = []
-        self.new_word()  # Создаём новое слово
-        return self.lay
-
-    def new_word(self):  # Новое слово
-        self.cages = []
-        self.letters = []
-        con = sqlite3.connect('content.sqlite3')
-        cur = con.cursor()
-        word = cur.execute("""SELECT id, answer FROM levels
-        WHERE done = 0""").fetchone()
-        try:
-            self.word = word[1]
-        except TypeError:
-            cur.execute("""UPDATE levels
-            SET done = 0""")
-            con.commit()
-            word = cur.execute("""SELECT id, answer FROM levels
-                    WHERE done = 0""").fetchone()
-        finally:
-            self.word = word[1]
-        con.close()
-        self.lay.add_widget(Image(source=f'data/{self.word}/1.png',
-                                  pos_hint={'x': 0.05, 'y': 0.7},
-                                  allow_stretch=True,
-                                  keep_ratio=False,
-                                  size_hint=(image_x, image_y)))
-        self.lay.add_widget(Image(source=f'data/{self.word}/2.png',
-                                  pos_hint={'x': 0.95 - image_x, 'y': 0.7},
-                                  allow_stretch=True,
-                                  keep_ratio=False,
-                                  size_hint=(image_x, image_y)))
-        self.lay.add_widget(Image(source=f'data/{self.word}/3.png',
-                                  pos_hint={'x': 0.05, 'y': 0.5},
-                                  allow_stretch=True,
-                                  keep_ratio=False,
-                                  size_hint=(image_x, image_y)))
-        self.lay.add_widget(Image(source=f'data/{self.word}/4.png',
-                                  pos_hint={'x': 0.95 - image_x, 'y': 0.5},
-                                  allow_stretch=True,
-                                  keep_ratio=False,
-                                  size_hint=(image_x, image_y)))
-        self.lay.add_widget(Label(text=f'{word[0]}',
-                                  pos_hint={'x': 0.35, 'y': 0.8},
-                                  size_hint=(0.3, 0.3),
-                                  font_size='30sp'))
-        self.new_empty_cages()
-        self.add_letters()
-
-    def new_empty_cages(self):
-        self.cage_coords = []
-        a = len(self.word)
-        for i in range(a):
-            space = 0.01
-            big_space = (1 - a * size_x - (a - 1) * space) / 2
-            if self.word[i] != ' ':
-                cage = EmptyCage(big_space + i * (size_x + space), 0.4, i)
-                self.cages.append(cage)
-                self.lay.add_widget(EmptyCage(big_space + i * (size_x + space), 0.4, i))
-
-    def add_letters(self):
-        b = 11
-        new_letters = random.choices(population=alphabet, k=b * 2)
-
-        let_nums = list(range(2 * b))
-        for i in range(len(self.word)):
-            if self.word[i] == ' ':
-                continue
-            a = random.choice(let_nums)
-            new_letters[a] = self.word[i]
-            let_nums.remove(a)
-
-        space_1 = (1 - b * size_x) / (b + 1)
-        for i in range(b):
-            a = LiterButton((i + 1) * space_1 + i * size_x, 0.3, new_letters[i])
-            a.bind(on_press=self.callback)
-            self.lay.add_widget(a)
-            self.letters.append(a)
-        for i in range(b):
-            a = LiterButton((i + 1) * space_1 + i * size_x, 0.2, new_letters[i + b])
-            a.bind(on_press=self.callback)
-            self.lay.add_widget(a)
-            self.letters.append(a)
-
-    def callback(self, instance):
-        if not instance.working:
-            return
-        if not instance.clicked:
-            for i in self.cages:
-                if not i.filled:
-                    instance.pos_hint = {'x': i.x_, 'y': i.y_}
-                    i.filled = True
-                    instance.clicked = True
-                    instance.cage = i
-                    i.let = instance
-                    break
-        else:
-            instance.pos_hint = {'x': instance.x_, 'y': instance.y_}
-            instance.cage.filled = False
-            instance.cage.let = None
-            instance.cage = None
-            instance.clicked = False
-
-        self.check_win()
-
-    def help_callback(self, instance):
-        if not instance.working:
-            return
-        possible = list(filter(lambda x: not x.filled, self.cages))
-        try:
-            chosen = random.choice(possible)
-        except IndexError:
-            for i in self.cages:
-                if not i.let.working:
-                    continue
-                if i.let.letter != self.word[i.num]:
-                    self.callback(i.let)
-                    break
-        else:
-            done = False
-            liter = ''.join(self.word.split())[chosen.num]
-            for i in self.letters:
-                if i.clicked:
-                    continue
-                if i.text == liter:
-                    i.pos_hint = {'x': chosen.x_, 'y': chosen.y_}
-                    i.working = False
-                    i.background_color = (0, 0.3, 0, 1)
-                    chosen.alpha_zero()
-                    self.lay.remove_widget(chosen)
-                    i.clicked = True
-                    i.cage = chosen
-                    i.cage.filled = True
-                    i.cage.let = i
-                    done = True
-                    break
-            if not done:
-                for i in self.cages:
-                    if not i.let.working:
-                        continue
-                    if i.let.letter != self.word[i.num]:
-                        self.callback(i.let)
-                        break
-
-            self.check_win()
-
-    def check_win(self):
-        cur_word = ''
-        for i in self.cages:
-            try:
-                a = i.let.letter
-                cur_word += a
-            except AttributeError:
-                break
-        if cur_word == ''.join(self.word.split()):
-            for i in self.letters:
-                i.working = False
-            self.help_button.working = False
-            Clock.schedule_once(self.win, 1)
-
-    def win(self, dt):
-        con = sqlite3.connect('content.sqlite3')
-        cur = con.cursor()
-        cur.execute(f"""UPDATE levels
-        SET done = 1
-        WHERE answer = ?""", (self.word,))
-        con.commit()
-        con.close()
-        self.new_word()
-        self.help_button.working = True
+        return Root()
 
 
 if __name__ == '__main__':
